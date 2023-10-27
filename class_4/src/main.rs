bea4c6ec1567062c9d075c0baf2103bf7dcae7d2
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-10-27 23:24:02 +0100

    variable immutability

---
+++ class_4/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x: String = String::from("Hello World");
+    println!("The value of x here is {x}");
+    let x: u8 = 9;
+    println!("The value of x is {}", x);
+    let m = the_name_of_the_function(2, 4);
+    println!("The function returned {m}");
+}
+
+fn the_name_of_the_function(x:u8, y:u8) -> u16 {
+    let z: u16 = (x * y).into();
+    z
+}
